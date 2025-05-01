85d8ceaeae4d946bddd26d9203526ea561640af5
FisherBlumenfeld <fisher@FisherSP6.>
2025-05-01 16:43:06 -0400

    commented code for MazeBoard and MazeBlock classes

--- maze.py
+++ maze.py
@@ -8,14 +8,14 @@ y_offset = 10
 class MazeBlock:
     def __init__(self):
         self.visited = False
-        self.possibleMoves = [] #when a square is visited, append -1 * step to get there, when it visits, append step taken
-    def __str__(self):
+        self.possibleMoves = [] #stores tuples of potential (x,y) changes out of this square. ie: left = (-1,0)
+    def __str__(self): #string func, used for debugging
         return str(self.visited)
-    def visit(self):
+    def visit(self): #sets visit = True for use in generation algo
         self.visited = True
-    def addPosMove(self,move):
+    def addPosMove(self,move): #stores valid moves out of this square
         self.possibleMoves.append(move)
-    def posMoves(self):
+    def posMoves(self): 
         return(self.possibleMoves)
 
         
@@ -27,11 +27,11 @@ class MazeBoard:
         for i in range(0,height):
             self.board.append([])
             for j in range(0,width):
-                self.board[i].append(MazeBlock())
+                self.board[i].append(MazeBlock()) #fills board with lists of MazeBlocks according to height and width
         
         #pygame stuff
         pygame.init()
-        self.screen = pygame.display.set_mode((wW,wH))
+        self.screen = pygame.display.set_mode((wW,wH)) #initializes pygame screen
         pygame.display.set_caption("Maze")
         for y in range(0,self.height):
             for x in range(0,self.width):
@@ -39,25 +39,25 @@ class MazeBoard:
 
         pygame.display.flip()
 
-    def __str__(self):
+    def __str__(self): #string function, used for debugging
         for i in range(0,self.height):
             for j in range(0,self.width):
-                print(self.board[i][j], end = " ")
+                print(self.board[i][j], end = " ") 
             print()
         return ""
-    def availableNeighbors(self,x,y):
+    def availableNeighbors(self,x,y): #adds tuples of potential moves to neighbors list
         neighbors = []
-        if (x > 0) and not (self.board[y][x-1].visited):
+        if (x > 0) and not (self.board[y][x-1].visited): #leftward
             neighbors.append((-1,0))
-        if (x < self.width - 1) and not (self.board[y][x+1].visited):
+        if (x < self.width - 1) and not (self.board[y][x+1].visited): #rightward
             neighbors.append((1,0))
-        if (y > 0) and not (self.board[y-1][x].visited):
+        if (y > 0) and not (self.board[y-1][x].visited): #upward
             neighbors.append((0,-1))
-        if (y < self.height-1) and not (self.board[y+1][x].visited):
-            neighbors.append((0,1))
+        if (y < self.height-1) and not (self.board[y+1][x].visited): #downward
+            neighbors.append((0,1)) 
         return neighbors
-    def recursiveGen(self,x,y,delay):
-        self.board[y][x].visit()
+    def recursiveGen(self,x,y,delay): #generates maze and updates connections in posMoves all blocks as well as updating graphics of screen to demonstrate algo
+        self.board[y][x].visit() #sets to visited to remove from possible neighbors
         while (self.availableNeighbors(x,y) != []):
             step = random.choice(self.availableNeighbors(x,y)) #picks random available neighbor to step to
             self.board[step[1] + y][step[0] + x].visit() #marks stepped to block as visited
@@ -73,11 +73,11 @@ class MazeBoard:
 
             
             if -1 not in step: #connects stepped to box 
-                pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(x_offset+20*x + 2,y_offset+20*y + 2, 16+4*step[0], 16+4*step[1])) 
+                pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(x_offset+20*x + 2,y_offset+20*y + 2, 16+4*step[0], 16+4*step[1])) #draws connections to rightward and downward blocks
             elif step == (-1,0):
-                pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(x_offset+20*x - 2,y_offset+20*y + 2, 4, 16))
+                pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(x_offset+20*x - 2,y_offset+20*y + 2, 4, 16)) #draws connection to leftward block
             else:
-                pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(x_offset+20*x + 2,y_offset+20*y - 2, 16, 4))
+                pygame.draw.rect(self.screen, (255,255,255), pygame.Rect(x_offset+20*x + 2,y_offset+20*y - 2, 16, 4)) #draws connection to upward block
             
             pygame.display.flip()
 
@@ -121,7 +121,7 @@ windowWidth = 0
 delay = 0.01
 
 difficulty = input("Choose a difficulty (demo, E, M, or H): ")
-while difficulty not in ["E", "M", "H","demo"]:
+while difficulty not in ["E", "M", "H","demo"]: #setting difficulty with user input
     difficulty = input("Choose a difficulty (demo, E, M, or H): ")
 if difficulty == "E":
     height = 9
@@ -135,29 +135,29 @@ elif difficulty == "H":
 elif difficulty == "demo":
     height = 18
     width = 24
-    delay = 0.2
+    delay = 0.1 #slows down generation algorithm for easier viewing
 
 windowHeight = 2*y_offset + 20*height
-windowWidth = 2*x_offset + 20*width
+windowWidth = 2*x_offset + 20*width #sets window dimensions based on maze dimensions
 
 while game: #while loop for infinite games to be played
-    maze = MazeBoard(width,height,windowHeight,windowWidth)
+    maze = MazeBoard(width,height,windowHeight,windowWidth) #initializes maze
     time.sleep(1)
     maze.recursiveGen(random.randint(0, width-1), random.randint(0, height-1),delay) #starts maze generation
-    pygame.draw.rect(maze.screen,(0,255,0),pygame.Rect(x_offset+20*(width-1) + 2,y_offset+20*(height-1) + 2, 16, 16))
-    player1 = player(maze)
+    pygame.draw.rect(maze.screen,(0,255,0),pygame.Rect(x_offset+20*(width-1) + 2,y_offset+20*(height-1) + 2, 16, 16)) #draws goal in botton right of maze
+    player1 = player(maze) #initializes player
     run = True     
     while run: #while loop for events in each individual maze
         for event in pygame.event.get():
-            if event.type == pygame.QUIT: #shutdown the game when window is closed
+            if event.type == pygame.QUIT: #end game after window closed
                 run = False
                 game = False
         
             if event.type == pygame.KEYDOWN: #register keystroke
-                player1.move(event.key)
+                player1.move(event.key) 
         
         if player1.xCoord == width-1 and player1.yCoord == height-1: #check for player reaching end of the maze
             run = False
-        pygame.display.update()
+        pygame.display.update() #updates screen to reflect movements
 
 pygame.quit()
